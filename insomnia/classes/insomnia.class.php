a2a85e983a4be08e426fd51fd7e07c9216fdc9de
Alex Safonov <me@asafonov.org>
2013-10-10 13:14:23 +0400

    Adding ability not to save some users (bots and spiders) to statistics

--- insomnia/classes/insomnia.class.php
+++ insomnia/classes/insomnia.class.php
@@ -12,10 +12,12 @@ class insomnia extends activeRecord {
     public $country;
     public $city;
     private $_geoipProvider;
+    private $_ignore = array();
 
     public function __construct() {
         parent::__construct();
         $this->_geoipProvider = registry::getInstance()->getService('geoipProvider');
+        $this->_ignore = config::getValue('insomnia_ignore');
     }
 
     public function create() {
@@ -24,12 +26,23 @@ class insomnia extends activeRecord {
         $this->month = date('m', $this->timestamp);
         $this->day = date('d', $this->timestamp);
         $this->ip = $_SERVER['REMOTE_ADDR'];
+        if (isset($this->_ignore['ip'])) {
+            foreach($this->_ignore['ip'] as $item) {
+                if (preg_match($item, $this->ip)) return false;
+            }
+        }
         $this->user_agent = $_SERVER['HTTP_USER_AGENT'];
+        if (isset($this->_ignore['user_agent'])) {
+            foreach($this->_ignore['user_agent'] as $item) {
+                if (preg_match($item, $this->user_agent)) return false;
+            }
+        }
         $this->request_uri = $_SERVER['REQUEST_URI'];
         $this->cookies = registry::getInstance()->getService('request')->cookie;
         $this->country = $this->_geoipProvider->getCountry($this->ip);
         $this->city = $this->_geoipProvider->getCity($this->ip);
         $this->save();
+        return true;
     }
 }
 
